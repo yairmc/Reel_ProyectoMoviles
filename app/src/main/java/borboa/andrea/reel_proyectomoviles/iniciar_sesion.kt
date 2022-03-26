83a7a36cc4e088f79fc70622faaaaa84394a3674
Andrea Borboa Ramírez <andrea.borboar@potros.itson.edu.mx>
2022-03-25 17:37:23 -0700

    Se agrego el layout activity_registrarse.xml

--- app/src/main/java/borboa/andrea/reel_proyectomoviles/iniciar_sesion.kt
+++ app/src/main/java/borboa/andrea/reel_proyectomoviles/iniciar_sesion.kt
@@ -1,11 +1,22 @@
 package borboa.andrea.reel_proyectomoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class iniciar_sesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
+        var btn_register: Button = findViewById(R.id.btn_register) as Button
+
+        btn_register.setOnClickListener{
+            var intent:Intent= Intent(this, registrarse::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
