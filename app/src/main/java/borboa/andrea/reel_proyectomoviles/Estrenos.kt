dbb8103222280de5e7b128febd35f93a27213124
Ricardo Yair Martinez Vazquez <98051181+yairmc@users.noreply.github.com>
2022-04-04 19:38:49 -0700

    Finalizacion de la parte de Estrenos

--- app/src/main/java/borboa/andrea/reel_proyectomoviles/Estrenos.kt
+++
@@ -1,6 +0,0 @@
-package borboa.andrea.reel_proyectomoviles
-
-data class Estrenos (val image:Int=0,
-                     val titulo:String="",
-                     val subtitulo:String="",
-                     val sinopsis:String="")
