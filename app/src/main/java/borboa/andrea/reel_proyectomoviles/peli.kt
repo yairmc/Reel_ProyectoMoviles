8d9c26497bf5331dfdb194eff4eadbd3ef1db3b6
Andrea Borboa Ramírez <andrea.borboar@potros.itson.edu.mx>
2022-04-05 16:57:50 -0700

    Agregada cartelera
    falta horarios y comentarios

---
+++ app/src/main/java/borboa/andrea/reel_proyectomoviles/peli.kt
@@ -0,0 +1,13 @@
+package borboa.andrea.reel_proyectomoviles
+
+data class peli (var imagen:Int,
+                 var titulo:String,
+                 var categoria:String,
+                 var subtitulo:String,
+                 var clasificacion:String,
+                 var duracion:String,
+                 var director:String,
+                 var reparto:String,
+                 var videoUrl: String?,
+                 var sinopsis:String,
+                 var comentario:ArrayList<calificaciones>)
